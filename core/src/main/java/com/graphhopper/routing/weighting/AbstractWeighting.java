0742e3b93cdfea8008bcd5fdaad0efd2a1030b93
Adam Rousell <adamrousell@hotmail.com>
2017-11-23 13:21:59 +0100

    Allowed limiting of speed based on user parameter

--- core/src/main/java/com/graphhopper/routing/weighting/AbstractWeighting.java
+++ core/src/main/java/com/graphhopper/routing/weighting/AbstractWeighting.java
@@ -20,19 +20,24 @@ package com.graphhopper.routing.weighting;
 import com.graphhopper.routing.util.FlagEncoder;
 import com.graphhopper.routing.util.HintsMap;
 import com.graphhopper.util.EdgeIteratorState;
+import com.graphhopper.util.PMap;
 
 /**
  * @author Peter Karich
  */
 public abstract class AbstractWeighting implements Weighting {
     protected final FlagEncoder flagEncoder;
+    protected double userMaxSpeed = -1;     // Limit maximum speed to that set by the user (i.e. for special material transport)
 
-    protected AbstractWeighting(FlagEncoder encoder) {
+    protected AbstractWeighting(FlagEncoder encoder, PMap map) {
         this.flagEncoder = encoder;
         if (!flagEncoder.isRegistered())
             throw new IllegalStateException("Make sure you add the FlagEncoder " + flagEncoder + " to an EncodingManager before using it elsewhere");
         if (!isValidName(getName()))
             throw new IllegalStateException("Not a valid name for a Weighting: " + getName());
+        if(map != null) {
+            userMaxSpeed = map.getDouble("max_speed", -1);
+        }
     }
 
     @Override
@@ -48,6 +53,11 @@ public abstract class AbstractWeighting implements Weighting {
             throw new IllegalStateException("Invalid speed stored in edge! " + speed);
         if (speed == 0)
             throw new IllegalStateException("Speed cannot be 0 for unblocked edge, use access properties to mark edge blocked! Should only occur for shortest path calculation. See #242.");
+
+        if (userMaxSpeed > 0) {
+            if (speed > userMaxSpeed)
+                speed = userMaxSpeed;
+        }
 
         return (long) (edgeState.getDistance() * 3600 / speed);
     }
